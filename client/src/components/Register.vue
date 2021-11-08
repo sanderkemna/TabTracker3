51de46bad47b6d312741d5dedac89d7dbfcb398a
Sander Kemna <kemna.sander@gmail.com>
2021-11-08 16:56:31 +0100

    working with vue2

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <br />
+    <input type="password" name="password" v-model="password" placeholder="password" />
+    <br />
+    <div v-html="error" class="error"/>
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register () {
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .error {
+    color: red;
+  }
+</style>
